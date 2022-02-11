52e9f8fce79c1c745213503d6dea336da5ab95b3
vancomm <vanushnick@gmail.com>
2022-02-11 03:59:57 +0300

    Basic functionality implemented

--- bot-commands.js
+++ bot-commands.js
@@ -1,9 +1,17 @@
 const { Telegraf } = require("telegraf");
 
 const initBotCommands = (bot) => {
-  bot.start((ctx) => ctx.reply('Hello!'));
+  bot.start((ctx) => {
+    return ctx.reply("Hello!")
+  });
 
-  // bot.help((ctx) => ctx.reply('Work in progress!'));
+  bot.help((ctx) => {
+    return ctx.reply('Work in progress!')
+  });
+
+  bot.on("text", (ctx) => {
+    return ctx.reply(ctx.message.text);
+  })
 };
 
 module.exports = {
--- handler.js
+++ handler.js
@@ -2,25 +2,52 @@
 
 require("dotenv").config();
 const { Telegraf } = require("telegraf");
-const { initBotCommands } = require("./bot-commands.js");
+// const { initBotCommands } = require("./bot-commands.js");
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-initBotCommands(bot);
+bot.start((ctx) => {
+  return ctx.reply("Hello!")
+});
+
+bot.help((ctx) => {
+  return ctx.reply('Work in progress!')
+});
+
+bot.command("secret", (ctx) => {
+  return ctx.replyWithMarkdownV2('||Аня и Лера красотки||');
+});
+
+bot.on("text", (ctx) => {
+  return ctx.reply(ctx.message.text);
+});
+
+// initBotCommands(bot);
+
+// bot.launch();
 
 module.exports.hello = async (event) => {
   const response = {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     body: '',
   };
 
-  if (!event.body) return response;
+  if (!event.body) {
+    response.statusCode = 404;
+    response.body = 'Not Found';
+    return response;
+  }
 
   try {
     const update = JSON.parse(event.body);
     await bot.handleUpdate(update);
   } catch (err) {
     console.log(err);
+    response.statusCode = 404;
+    response.body = 'Not Found';
   }
 
   return response;
@@ -29,30 +56,36 @@ module.exports.hello = async (event) => {
 module.exports.setWebhook = async (event) => {
   const response = {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     body: '',
   };
-
+  const url = `https://${event.headers.Host}/${event.requestContext.stage}/webhook`;
   try {
-    const url = process.env.AWS_ENDPOINT;
     await bot.telegram.setWebhook(url);
   } catch (err) {
     console.log(err);
+    response.statusCode = 404;
+    response.body = 'Not Found';
   }
-
   return response;
 };
 
 module.exports.deleteWebhook = async (event) => {
   const response = {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     body: '',
   };
-
   try {
     await bot.telegram.deleteWebhook({drop_pending_updates: true});
   } catch (err) {
     console.log(err);
+    response.statusCode = 404;
+    response.body = 'Not Found';
   }
-
   return response;
 };
